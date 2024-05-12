501119dad437bb2db3befbde81899c6efd90be00
bqxhweb <3200232561@qq.com>
2024-05-12 10:46:30 +0800

    Site updated: 2024-05-12 10:46:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/11/我们的论坛/"];function toRandomPost(){
+var posts=["2024/05/11/我们的论坛/","2024/05/12/八泉学会关于匿名37的看法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
